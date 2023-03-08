ce34927c5357e0e36ba85297f36ae82e90b3acdb
Sergio Peralta <seperalta88@gmail.com>
2023-03-08 11:10:39 -0300

    added create forms

--- test/controllers/product_controller_test.rb
+++ test/controllers/product_controller_test.rb
@@ -1,8 +1,15 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get product_index_url
+  test 'should get index' do
+    get products_path
     assert_response :success
+    assert_select '.product', 2 # 2 products in fixtures
+  end
+
+  test 'should render a detailed product page' do
+    get product_path(products(:PS5))
+    assert_response :success
+    assert_select 'h1', products(:PS5).title
   end
 end
