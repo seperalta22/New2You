09af744083e9ebc873f55648f552ba0faf3bb303
Sergio Peralta <seperalta88@gmail.com>
2023-06-17 14:29:56 -0300

    add some translation

---
+++ commands.md
@@ -0,0 +1,5 @@
+# github.
+
+git add . 
+git commit -m "add some translation"
+git push 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories
+  resources :categories, except: :show
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :products, path: '/'
 
